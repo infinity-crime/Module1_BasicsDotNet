0543c7df0865e0369395d6934619bf28e23a7ba9
Kirill Zhestkov <infinity.crime@outlook.com>
2025-10-22 18:40:20 +0300

    TASK-303: Сделано консольное приложение (6 подзадача), где создан простейший класс Book с приватными полями и публичными свойствами (без проверкок на корректный ввод). Дополнительно в Main сделана проверка.

---
+++ 6_DefaultClass/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_DefaultClass
+{
+    public class Book
+    {
+        private string _title;
+        private string _author;
+        private int _year;
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public string Author
+        {
+            get => _author;
+            set => _author = value;
+        }
+
+        public int Year
+        {
+            get => _year;
+            set => _year = value;
+        }
+
+        public Book(string title, string author, int year)
+        {
+            _title = title;
+            _author = author;
+            _year = year;
+        }
+    }
+}
---
+++ 6_DefaultClass/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _6_DefaultClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // проверка класса Book
+            var book = new Book("Techcore Development", "Kirill", 2025);
+
+            Console.WriteLine($"Назавние: {book.Title}");
+            Console.WriteLine($"Автор: {book.Author}");
+            Console.WriteLine($"Год: {book.Year}");
+        }
+    }
+}
