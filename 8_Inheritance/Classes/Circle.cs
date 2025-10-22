c5895af020ba53dd129cd82005d26052bc037675
Kirill Zhestkov <infinity.crime@outlook.com>
2025-10-22 20:44:18 +0300

    TASK-303: сделано консольное приложение (8 подзадача), которое демонстрирует работу наследования и полиморфизма.

---
+++ 8_Inheritance/Classes/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _8_Inheritance.Classes.Common;
+
+namespace _8_Inheritance.Classes
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
