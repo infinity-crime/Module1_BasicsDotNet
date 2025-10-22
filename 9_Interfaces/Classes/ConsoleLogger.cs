00d1f9278fb36ba9df635da0191f3ad1e0c5967e
Kirill Zhestkov <infinity.crime@outlook.com>
2025-10-22 20:54:25 +0300

    TASK-303: реализовано консольное приложение (9 подзадача), демонстрирующее работу с интерфейсом.

---
+++ 9_Interfaces/Classes/ConsoleLogger.cs
@@ -0,0 +1,17 @@
+﻿using _9_Interfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Interfaces.Classes
+{
+    public class ConsoleLogger : ILoggable
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log in Console: {message}");
+        }
+    }
+}
