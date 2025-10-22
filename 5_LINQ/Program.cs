76be73cd96026c79f7faa79ce0d297cdfa95c722
Kirill Zhestkov <infinity.crime@outlook.com>
2025-10-22 18:28:52 +0300

    TASK-303: Добавлено консольное приложение для работы с LINQ выражениями (5 подзадача)

---
+++ 5_LINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _5_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = [23, 55, 12, 2, 1, 56, 1234, 2, 4, 5, 6, 7, 8, 345, 21];
+
+            var result = numbers.Where(x => x % 2 == 0)
+                .Select(x => x * 2)
+                .OrderByDescending(x => x);
+
+            ShowList(result);
+        }
+
+        static void ShowList(IEnumerable<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+        }
+    }
+}
